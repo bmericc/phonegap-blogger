d673214cf5c3d48ae27bbdea08f73fadb7f88a16
Bahri Meriç CANLI <bahri@bahri.info>
2015-01-02 20:11:06 +0200

    splash olayları

--- js/index.js
+++ js/index.js
@@ -1,31 +1,6 @@
-var app = {
-
-    initialize: function() {
-        this.bindEvents();
-    },
-
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-
-    receivedEvent: function(id) {
-
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-
-    }
-
-};
+document.addEventListener('deviceready', function() {
+ 	setTimeout(function() { navigator.splashscreen.hide(); }, 3000);
+});  
 
 
 (function($) {
